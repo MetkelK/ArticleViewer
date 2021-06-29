a52f088e97318e0dfb62a9113890c48ee42cebd2
Metkel K <metkelk@gmail.com>
2021-06-29 17:14:56 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Article from './Components/Article'
+import TopStory from './Components/TopStory';
+import ViewedArticle from './Components/ViewedArticle';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 aria-label="test">test</h1>
+      <div>
+        <Article />
+        <TopStory />
+        <ViewedArticle />
+
+      </div>
     </div>
   );
 }
