9a107a887a8c3a31d58bf9a582e4811e27ef7c3d
Metkel K <metkelk@gmail.com>
2021-06-30 15:50:15 -0400

    added state and api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import Article from './Components/Article'
@@ -6,13 +6,36 @@ import TopStory from './Components/TopStory';
 import ViewedArticle from './Components/ViewedArticle';
 
 const App = () => {
+  const [query, setQuery] = useState<string | ''>('')
+  const [topStory, setTopStory] = useState<[]>([])
+  const [viewedStory, setViewedStory] = useState<[]>([])
+
+  const getArticles = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/home.json?&api-key=${process.env.REACT_APP_API_KEY}`
+    )
+    .then(res => res.json())
+    .then(res => setTopStory(res.results))
+    .catch(err => console.log(err))
+
+    fetch(
+      `https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?&api-key=${process.env.REACT_APP_API_KEY}`
+    )
+    .then(res => res.json())
+    .then(res => setViewedStory(res.results))
+    .catch(err => console.log(err))
+  }
+
   return (
     <div>
-      <h1 aria-label="test">test</h1>
+      <form onSubmit={getArticles}>
+        <input type="text" placeholder="Search" onChange={(e) => setQuery(e.target.value)} value={query}/>
+      </form>
       <div>
         <Article />
-        <TopStory />
-        <ViewedArticle />
+        <TopStory topstories={topStory}/>
+        <ViewedArticle viewedstories={viewedStory}/>
 
       </div>
     </div>
