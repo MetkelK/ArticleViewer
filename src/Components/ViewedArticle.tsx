a5e9166a9a48344413c2691ce1ccd00e311e7efd
Metkel K <metkelk@gmail.com>
2021-06-30 15:51:22 -0400

    added viewed articles component

--- src/Components/ViewedArticle.tsx
+++ src/Components/ViewedArticle.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
 
-const ViewedArticle = () => {
-    return (
-        <div>
-            <h1 aria-label="title">Title</h1>
-            <h2 aria-label="byline">Byline</h2>
-            <p aria-label="abstract">Abstract</p>
-            <p aria-label="date">Date</p>
-            <p aria-label="url">Url</p>
-            <img aria-label="image" src="" alt="" />
-        </div>
-    )
+interface Articles {
+    title: string;
+    byline: string;
+    abstract: string;
+    date: string;
+    url: string;
+}
+
+const ViewedArticle = ({viewedstories}:any) => {
+    if (viewedstories) {
+
+        return (
+            <div>
+                {viewedstories.map((article: Articles, i:number) => (
+                <div key={i}>
+                    <h1 aria-label="title">{article.title}</h1>
+                    <h2 aria-label="byline">{article.byline}</h2>
+                    <p aria-label="abstract">{article.abstract}</p>
+                    <p aria-label="date">{article.date}</p>
+                    <a href={article.url} aria-label="url">New York Times</a>
+                </div>
+            ))}
+            </div>
+        )
+    } else {
+    return null
+    }
 }
 
 export default ViewedArticle
