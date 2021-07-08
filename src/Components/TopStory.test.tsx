7e459b8b3b71176a98d88a3d8f95f8cb90b61757
Metkel K <metkelk@gmail.com>
2021-07-07 20:44:10 -0400

    added fake data to test

--- src/Components/TopStory.test.tsx
+++ src/Components/TopStory.test.tsx
@@ -2,44 +2,51 @@ import React from 'react'
 import TopStory from './TopStory'
 import { render, screen } from '@testing-library/react'
 
+const data = [{
+    title: "title",
+    byline: "byline",
+    abstract: "abstract",
+    date: "date",
+    url: "www.url.com",
+}]
+
 describe('Article', () => {
     it('Renders Article', () => {
-        render(<TopStory />)
+        render(<TopStory topstories={data}/>)
     })
 
     it('Article has Title', () => {
-        render(<TopStory />)
-        const title = screen.getByLabelText(/title/i)
+        render(<TopStory topstories={data}/>)
+        const title = screen.getByRole('heading', {
+            name: /title/i
+          })
         expect(title).toBeVisible()
     })
 
     it('Article has Byline', () => {
-        render(<TopStory />)
-        const byline = screen.getByLabelText(/byline/i)
+        render(<TopStory topstories={data}/>)
+        const byline = screen.getByRole('heading', {
+            name: /byline/i
+          })
         expect(byline).toBeVisible()
     })
 
     it('Article has Abstract', () => {
-        render(<TopStory />)
+        render(<TopStory topstories={data}/>)
         const abstract = screen.getByLabelText(/abstract/i)
         expect(abstract).toBeVisible()
     })
 
     it('Article has Date', () => {
-        render(<TopStory />)
+        render(<TopStory topstories={data}/>)
         const date = screen.getByLabelText(/date/i)
         expect(date).toBeVisible()
     })
 
     it('Article has Url', () => {
-        render(<TopStory />)
+        render(<TopStory topstories={data}/>)
         const url = screen.getByLabelText(/url/i)
         expect(url).toBeVisible()
     })
 
-    it('Article has Image', () => {
-        render(<TopStory />)
-        const image = screen.getByLabelText(/image/i)
-        expect(image).toBeVisible()
-    })
 })
