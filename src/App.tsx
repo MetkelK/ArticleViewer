16f7c16f953c6520be27825856a955737c71f3c3
Metkel K <metkelk@gmail.com>
2021-07-13 18:12:34 -0400

    added Modal component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,69 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import SearchedArticle from './Components/SearchedArticle';
-import TopStory from './Components/TopStory';
-import ViewedArticle from './Components/ViewedArticle';
+import SearchedArticle from "./Components/SearchedArticle";
+import TopStory from "./Components/TopStory";
+import ViewedArticle from "./Components/ViewedArticle";
+import Modal from "./Components/Modal";
 
 const App = () => {
-  const [query, setQuery] = useState<string | ''>('')
-  const [search, setSearch] = useState<boolean | false>(false)
-  const [searchedStory, setSearchedStory] = useState<[]>([])
-  const [topStory, setTopStory] = useState<[]>([])
-  const [viewedStory, setViewedStory] = useState<[]>([])
+  const [query, setQuery] = useState<string | "">("");
+  const [search, setSearch] = useState<boolean | false>(false);
+  const [searchedStory, setSearchedStory] = useState<[]>([]);
+  const [topStory, setTopStory] = useState<[]>([]);
+  const [viewedStory, setViewedStory] = useState<[]>([]);
 
   useEffect(() => {
     const getArticles = () => {
       fetch(
         `https://api.nytimes.com/svc/topstories/v2/home.json?&api-key=${process.env.REACT_APP_API_KEY}`
       )
-      .then(res => res.json())
-      .then(res => setTopStory(res.results))
-      .catch(err => console.log(err))
-  
+        .then((res) => res.json())
+        .then((res) => setTopStory(res.results))
+        .catch((err) => console.log(err));
+
       fetch(
         `https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?&api-key=${process.env.REACT_APP_API_KEY}`
       )
-      .then(res => res.json())
-      .then(res => setViewedStory(res.results))
-      .catch(err => console.log(err))
-    }
+        .then((res) => res.json())
+        .then((res) => setViewedStory(res.results))
+        .catch((err) => console.log(err));
+    };
     getArticles();
-  }, [])
+  }, []);
 
-  
-    const searchArticles = (e:React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-      setSearch(true)
-      fetch(
-        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.REACT_APP_API_KEY}`
-      )
-      .then(res => res.json())
-      .then(res => setSearchedStory(res.response.docs))
-      .catch(err => console.log(err))
-    }
-    
+  const searchArticles = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(true);
+    fetch(
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((res) => setSearchedStory(res.response.docs))
+      .catch((err) => console.log(err));
+  };
 
-// use below for form fetch
-// const formSubmit = (e:React.FormEvent<HTMLFormElement>) => {}
+  // use below for form fetch
+  // const formSubmit = (e:React.FormEvent<HTMLFormElement>) => {}
   return (
     <div>
       <form onSubmit={searchArticles}>
-        <input type="text" placeholder="Search" onChange={(e) => setQuery(e.target.value)} value={query}/>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
       </form>
       <div>
-        <SearchedArticle searchedstories={searchedStory} searchCompleted={setSearch}/>
-        <TopStory topstories={topStory}/>
-        <ViewedArticle viewedstories={viewedStory}/>
-
+        <Modal search={search} setSearch={setSearch}>
+          <SearchedArticle searchedstories={searchedStory} />
+        </Modal>
+        <TopStory topstories={topStory} />
+        <ViewedArticle viewedstories={viewedStory} />
       </div>
     </div>
   );
-}
+};
 
 export default App;
