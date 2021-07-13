5dad2051a24584effe281795c73a8c5d71085fb1
Metkel K <metkelk@gmail.com>
2021-07-13 18:20:16 -0400

    expanded interface and added images

--- src/Components/ViewedArticle.tsx
+++ src/Components/ViewedArticle.tsx
@@ -1,31 +1,54 @@
-import React from 'react'
+import React from "react";
 
 interface Articles {
-    title: string;
-    byline: string;
-    abstract: string;
-    date: string;
-    url: string;
+  title: string;
+  byline: string;
+  abstract: string;
+  published_date: string;
+  url: string;
+  media: {
+    length: number;
+    [0]: {
+      ["media-metadata"]: {
+        [2]: {
+          url: string;
+        };
+      };
+      caption: string;
+    };
+  };
 }
 
-const ViewedArticle = ({viewedstories}:any) => {
-    if (viewedstories) {
-        return (
-            <div>
-                {viewedstories.map((article: Articles, i:number) => (
-                <div key={i}>
-                    <h1 aria-label="title">{article.title}</h1>
-                    <h2 aria-label="byline">{article.byline}</h2>
-                    <p aria-label="abstract">{article.abstract}</p>
-                    <p aria-label="date">{article.date}</p>
-                    <a href={article.url} aria-label="url">New York Times</a>
+const ViewedArticle = ({ viewedstories }: any) => {
+  if (viewedstories) {
+    return (
+      <div className="article-container">
+        {viewedstories.map((article: Articles, i: number) => {
+          if (article.media.length > 0)
+            return (
+              <div key={i} className="article">
+                <img
+                  src={article.media[0]["media-metadata"][2].url}
+                  alt={article.media[0].caption}
+                />
+                <div className="info-container">
+                  <h1 aria-label="title">{article.title}</h1>
+                  <h2 aria-label="byline">{article.byline}</h2>
+                  <p aria-label="abstract">{article.abstract}</p>
+                  <p aria-label="date">{article.published_date}</p>
+                  <a href={article.url} aria-label="url">
+                    Read More
+                  </a>
                 </div>
-            ))}
-            </div>
-        )
-    } else {
-    return null
-    }
-}
+              </div>
+            );
+          return null;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
-export default ViewedArticle
+export default ViewedArticle;
