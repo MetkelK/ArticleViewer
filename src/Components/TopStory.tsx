960b0cbab0be607b84471e2a788352eb8d41cc8e
Metkel K <metkelk@gmail.com>
2021-07-13 18:19:17 -0400

    fixed formatting

--- src/Components/TopStory.tsx
+++ src/Components/TopStory.tsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 
 interface Articles {
-    title: string;
-    byline: string;
-    abstract: string;
-    date: string;
-    url: string;
+  title: string;
+  byline: string;
+  abstract: string;
+  date: string;
+  url: string;
 }
 
-const TopStory = ({topstories}:any) => {
-    if (topstories) {
-        return (
-            <div>
-            {topstories.map((article: Articles, i:number) => (
-                <div key={i}>
-                    <h1 aria-label="title">{article.title}</h1>
-                    <h2 aria-label="byline">{article.byline}</h2>
-                    <p aria-label="abstract">{article.abstract}</p>
-                    <p aria-label="date">{article.date}</p>
-                    <a href={article.url} aria-label="url">New York Times</a>
-                </div>
-            ))}
-            </div>
-        )   
-    } else {
-        return null
-    }
+const TopStory = ({ topstories }: any) => {
+  if (topstories) {
+    return (
+      <div>
+        {topstories.map((article: Articles, i: number) => (
+          <div key={i}>
+            <h1 aria-label="title">{article.title}</h1>
+            <h2 aria-label="byline">{article.byline}</h2>
+            <p aria-label="abstract">{article.abstract}</p>
+            <p aria-label="date">{article.date}</p>
+            <a href={article.url} aria-label="url">
+              New York Times
+            </a>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
-}
-
-export default TopStory
+export default TopStory;
