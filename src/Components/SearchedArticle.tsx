5eccd9ee295e43f0945b104ed1cac4a29b169a31
Metkel K <metkelk@gmail.com>
2021-07-13 18:18:41 -0400

    fixed formatting

--- src/Components/SearchedArticle.tsx
+++ src/Components/SearchedArticle.tsx
@@ -1,37 +1,38 @@
-import React from 'react'
+import React from "react";
 
 interface Articles {
-    _id: string;
-    headline:{
-        main: string;
-    };
-    byline:{
-        original: string;
-    };
-    abstract: string;
-    pub_date: string;
-    web_url: string;
+  _id: string;
+  headline: {
+    main: string;
+  };
+  byline: {
+    original: string;
+  };
+  abstract: string;
+  pub_date: string;
+  web_url: string;
 }
 
-const SearchedArticle = ({searchedstories, setSearch}:any) => {
-    if (searchedstories) {
+const SearchedArticle = ({ searchedstories }: any) => {
+  if (searchedstories) {
+    return (
+      <div>
+        {searchedstories.map((article: Articles, i: number) => (
+          <div key={i}>
+            <h1 aria-label="title">{article.headline.main}</h1>
+            <h2 aria-label="byline">{article.byline.original}</h2>
+            <p aria-label="abstract">{article.abstract}</p>
+            <p aria-label="date">{article.pub_date}</p>
+            <a href={article.web_url} aria-label="url">
+              New York Times
+            </a>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
-        return (
-            <div>
-                {searchedstories.map((article: Articles, i:number) => (
-                <div key={i}>
-                    <h1 aria-label="title">{article.headline.main}</h1>
-                    <h2 aria-label="byline">{article.byline.original}</h2>
-                    <p aria-label="abstract">{article.abstract}</p>
-                    <p aria-label="date">{article.pub_date}</p>
-                    <a href={article.web_url} aria-label="url">New York Times</a>
-                </div>
-            ))}
-            </div>
-    )
-    } else {
-        return null
-    }
-}
-
-export default SearchedArticle
+export default SearchedArticle;
