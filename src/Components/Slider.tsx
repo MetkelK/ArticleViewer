2957f438b583b9c98982192de269aa7ce24a3369
Metkel K <metkelk@gmail.com>
2021-07-21 23:40:24 -0400

    Created slider component

---
+++ src/Components/Slider.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import gsap from "gsap";
+import Scroll from "../utils/scroll";
+import { constants, instances } from "../utils/store";
+
+const Slider = () => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default Slider;
