ec2a19bf7fb63499ffd0dc4ece5bdcd7115ec7a8
Metkel K <metkelk@gmail.com>
2021-07-21 23:48:13 -0400

    removed comments

--- src/Components/ViewedArticle.tsx
+++ src/Components/ViewedArticle.tsx
@@ -31,39 +31,9 @@ const ViewedArticle = ({ viewedstories, guardian }: any) => {
   }
   let combinedArticles = combined.filter(Boolean);
 
-  console.log(combinedArticles);
-
   if (viewedstories || guardian) {
     return (
       <div className="article-container">
-        {/* {viewedstories.map((article: Articles, i: number) => {
-          if (article.media.length > 0)
-            return (
-              <div key={i} className="article">
-                <a href={article.url} aria-label="url">
-                  <figure>
-                    <img
-                      src={article.media[0]["media-metadata"][2].url}
-                      alt={article.media[0].caption}
-                    />
-                    <figcaption className="copyright">
-                      {article.media[0].copyright}
-                    </figcaption>
-                  </figure>
-                </a>
-                <a
-                  href={article.url}
-                  aria-label="url"
-                  className="info-container"
-                >
-                  <h3 aria-label="title">{article.title}</h3>
-                </a>
-
-                <p aria-label="abstract">{article.abstract}</p>
-              </div>
-            );
-          return null;
-        })} */}
         {combinedArticles.map((article: Articles) => {
           if (typeof article.id === "number") {
             if (article.media.length > 0) {
