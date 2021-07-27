1e6d7d050ba5bd0981ec5207c924603f9c7d367c
Metkel K <metkelk@gmail.com>
2021-07-26 23:19:22 -0400

    updated component

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import gsap from "gsap";
 import Scroll from "../utils/scroll";
 import { constants, instances } from "../utils/store";
 
 const Slider = () => {
   return (
-    <div>
-      <h1>test</h1>
+    <div className="slider">
+      <div className="container" data-scroll>
+        <div className="item">
+          <div className="img-wrap">
+            <img
+              src="https://images.unsplash.com/photo-1472835560847-37d024ebacdc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&ar=0.8"
+              alt=""
+            />
+          </div>
+
+          <div className="item-container">
+            <div className="item-heading-wrap">
+              <div className="item-heading">
+                <h3>Test</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="item-button-wrap">
+            <div className="item-button">read</div>
+          </div>
+        </div>
+      </div>
+      <div className="slider-progress-wrap">
+        <div className="slider-progress"></div>
+      </div>
     </div>
   );
 };
