0078c1d64c7601358e2bd54d0491f01bf42770a7
Metkel K <metkelk@gmail.com>
2021-07-29 23:48:28 -0400

    updated top story component

--- src/Components/TopStory.tsx
+++ src/Components/TopStory.tsx
@@ -4,8 +4,17 @@ interface Articles {
   title: string;
   byline: string;
   abstract: string;
-  date: string;
   url: string;
+  multimedia: {
+    length: number;
+    [0]: {
+      caption: string;
+      copyright: string;
+      height: number;
+      width: number;
+      url: string;
+    };
+  };
 }
 
 const TopStory = ({ topstories }: any) => {
@@ -13,14 +22,41 @@ const TopStory = ({ topstories }: any) => {
     return (
       <div className="scroller">
         {topstories.map((article: Articles, i: number) => (
-          <div key={i} className="toparticle">
-            <h1 aria-label="title">{article.title}</h1>
-            <h2 aria-label="byline">{article.byline}</h2>
-            <p aria-label="abstract">{article.abstract}</p>
-            <p aria-label="date">{article.date}</p>
-            <a href={article.url} aria-label="url">
-              New York Times
+          // <div key={i} className="toparticle">
+          //   <h1 aria-label="title">{article.title}</h1>
+          //   <h2 aria-label="byline">{article.byline}</h2>
+          //   <p aria-label="abstract">{article.abstract}</p>
+          //   <p aria-label="date">{article.date}</p>
+          //   <a href={article.url} aria-label="url">
+          //     New York Times
+          //   </a>
+          // </div>
+          <div key={i} className="">
+            <a
+              href={article.url}
+              aria-label="url"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <figure>
+                <img
+                  src={article.multimedia[0].url}
+                  alt={article.multimedia[0].caption}
+                />
+                <figcaption className="copyright">
+                  {article.multimedia[0].copyright}
+                </figcaption>
+              </figure>
             </a>
+            <a
+              href={article.url}
+              aria-label="url"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h3 aria-label="title">{article.title}</h3>
+            </a>
+            <p aria-label="abstract">{article.abstract}</p>
           </div>
         ))}
       </div>
