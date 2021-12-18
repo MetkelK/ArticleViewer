d74bd6f2c46fde5abccb7e21a85c86800f8b16bd
Metkel K <metkelk@gmail.com>
2021-12-17 23:43:14 -0500

    changed headline

--- src/Components/TopStory.tsx
+++ src/Components/TopStory.tsx
@@ -41,8 +41,8 @@ const TopStory = ({ topstories, headline }: any) => {
 
   if (topstories && headline) {
     return (
-      <div className="container mx-auto">
-        <div className="w-full">
+      <div className="">
+        <div className="mx-8 mb-12 bg-white shadow-md border border-gray-200 rounded-lg max-w-fit dark:bg-gray-800 dark:border-gray-700">
           <a
             href={headline.url}
             aria-label="url"
@@ -54,32 +54,41 @@ const TopStory = ({ topstories, headline }: any) => {
                 <img
                   src={headline.multimedia[0].url}
                   alt={headline.multimedia[0].caption}
+                  className="rounded-t-lg"
                 />
-                <figcaption className=" text-gray-400 text-xs leading-3 tracking-wide mt-2 text-right">
+                <figcaption className=" text-gray-400 text-xs leading-3 tracking-wide mt-2 mr-4 text-right">
                   {headline.multimedia[0].copyright}
                 </figcaption>
               </figure>
             )}
           </a>
+
+          <h3
+            aria-label="title"
+            className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white px-4"
+          >
+            {headline.title}
+          </h3>
+          <p
+            aria-label="abstract"
+            className=" text-gray-700 font-normal mb-3 dark:text-gray-400 px-4"
+          >
+            {headline.abstract}
+          </p>
           <a
             href={headline.url}
             aria-label="url"
             target="_blank"
             rel="noreferrer"
+            className="text-gray-900 font-bold mb-3 dark:text-white px-4"
           >
-            <h3
-              aria-label="title"
-              className=" text-3xl font-bold font-sans mb-2"
-            >
-              {headline.title}
-            </h3>
+            Read More
           </a>
-          <p aria-label="abstract">{headline.abstract}</p>
         </div>
 
-        <div className="scroller">
+        <div className="snap-x flex flex-row overflow-x-scroll">
           {topstories.map((article: Articles, i: number) => (
-            <div key={i} className="toparticle">
+            <div key={i} className="snap-center mr-4 [width:90vw]">
               <a
                 href={article.url}
                 aria-label="url"
@@ -90,6 +99,7 @@ const TopStory = ({ topstories, headline }: any) => {
                   <img
                     src={article.multimedia[0].url}
                     alt={article.multimedia[0].caption}
+                    className="w-full block object-cover h-full"
                   />
                   <figcaption className="copyright">
                     {article.multimedia[0].copyright}
