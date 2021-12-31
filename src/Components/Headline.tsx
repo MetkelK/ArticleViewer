8a62b54c426bcc50a83a3fd84c72bf6f22a90b78
Metkel K <metkelk@gmail.com>
2021-12-30 23:21:26 -0500

    changed prop name

--- src/Components/Headline.tsx
+++ src/Components/Headline.tsx
@@ -1,24 +1,29 @@
 import { Story } from "./Interfaces";
 
 interface ArticleProps {
-  story: Story;
+  headline: Story;
 }
 
-const Headline = ({ story }: ArticleProps) => {
-  if (story) {
+const Headline = ({ headline }: ArticleProps) => {
+  if (headline) {
     return (
       <div className="container mx-auto">
         <div className="mx-4 mb-12 pb-4 bg-white shadow-md border border-gray-200 rounded-lg max-w-fit dark:bg-gray-800 dark:border-gray-700">
-          <a href={story.url} aria-label="url" target="_blank" rel="noreferrer">
-            {story.multimedia && (
+          <a
+            href={headline.url}
+            aria-label="url"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {headline.multimedia && (
               <figure>
                 <img
-                  src={story.multimedia[0].url}
-                  alt={story.multimedia[0].caption}
+                  src={headline.multimedia[0].url}
+                  alt={headline.multimedia[0].caption}
                   className="rounded-t-lg"
                 />
                 <figcaption className="text-gray-400 text-xs leading-3 tracking-wide my-2 mr-4 text-right">
-                  {story.multimedia[0].copyright}
+                  {headline.multimedia[0].copyright}
                 </figcaption>
               </figure>
             )}
@@ -28,16 +33,16 @@ const Headline = ({ story }: ArticleProps) => {
             aria-label="title"
             className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white px-4"
           >
-            {story.title}
+            {headline.title}
           </h3>
           <p
             aria-label="abstract"
             className=" text-gray-700 font-normal mb-2 dark:text-gray-400 px-4"
           >
-            {story.abstract}
+            {headline.abstract}
           </p>
           <a
-            href={story.url}
+            href={headline.url}
             aria-label="url"
             target="_blank"
             rel="noreferrer"
