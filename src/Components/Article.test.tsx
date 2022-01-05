4a225c6ceeea3c1f0cc02db327162ade887d8b25
Metkel K <metkelk@gmail.com>
2022-01-05 00:24:01 -0500

    updated article test

--- src/Components/Article.test.tsx
+++ src/Components/Article.test.tsx
@@ -1,39 +1,56 @@
-import React from 'react'
-import Article from './Article'
-import { render, screen } from '@testing-library/react'
+import Article from "./Article";
+import { render, screen } from "@testing-library/react";
+import { Story } from "./Interfaces";
 
-describe('Article', () => {
-    it('Renders Article', () => {
-        render(<Article />)
-    })
+const story: Story = {
+  title: "title",
+  byline: "byline",
+  abstract: "abstract",
+  url: "url",
+  multimedia: {
+    length: 3,
+    0: {
+      caption: "caption",
+      copyright: "copyright",
+      height: 640,
+      width: 640,
+      url: "url",
+    },
+  },
+};
 
-    it('Article has Title', () => {
-        render(<Article />)
-        const title = screen.getByLabelText(/title/i)
-        expect(title).toBeVisible()
-    })
+describe("Article", () => {
+  it("Renders Article", () => {
+    render(<Article story={story} />);
+  });
 
-    it('Article has Byline', () => {
-        render(<Article />)
-        const title = screen.getByLabelText(/byline/i)
-        expect(title).toBeVisible()
-    })
+  it("Article has Title", () => {
+    render(<Article story={story} />);
+    const title = screen.getByLabelText(/title/i);
+    expect(title).toBeVisible();
+  });
 
-    it('Article has Abstract', () => {
-        render(<Article />)
-        const title = screen.getByLabelText(/abstract/i)
-        expect(title).toBeVisible()
-    })
+  it("Article has Byline", () => {
+    render(<Article story={story} />);
+    const title = screen.getByLabelText(/byline/i);
+    expect(title).toBeVisible();
+  });
 
-    it('Article has Author', () => {
-        render(<Article />)
-        const title = screen.getByLabelText(/author/i)
-        expect(title).toBeVisible()
-    })
+  it("Article has Abstract", () => {
+    render(<Article story={story} />);
+    const title = screen.getByLabelText(/abstract/i);
+    expect(title).toBeVisible();
+  });
 
-    it('Article has Date', () => {
-        render(<Article />)
-        const title = screen.getByLabelText(/date/i)
-        expect(title).toBeVisible()
-    })
-})
+  it("Article has Author", () => {
+    render(<Article story={story} />);
+    const title = screen.getByLabelText(/author/i);
+    expect(title).toBeVisible();
+  });
+
+  it("Article has Date", () => {
+    render(<Article story={story} />);
+    const title = screen.getByLabelText(/date/i);
+    expect(title).toBeVisible();
+  });
+});
